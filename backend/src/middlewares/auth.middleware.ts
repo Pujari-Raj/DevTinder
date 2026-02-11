22e87b853edd993cec8191ea1141aab6ea353d82
Basvaraj Pujari <basvarajpujari607@gmail.com>
2026-02-11 23:27:55 +0530

    add: added auth middlware base code

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,32 @@
+import { AsyncHandler } from "../utils/handlers";
+import jwt from 'jsonwebtoken'
+import { UserModel } from "../models/user.model";
+
+export const userAuth = AsyncHandler(async (req, res, next) => {
+    // getting token from cookies
+
+    const token = req?.cookies?.devTinderToken;
+
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "User Unauthorized"
+        })
+    }
+
+    // if token is avaiable then check if user is available
+    const decodedUser = jwt.verify(token, process.env.JWT_SECRET as string) as { id: string };
+
+    // checking if user is there in DB
+    const user = UserModel.findById(decodedUser?.id);
+
+    if (!user) {
+        return res.status(401).json({
+            success: false,
+            message: 'User Not Found'
+        })
+    }
+
+    (req as any).user = user;
+    next();
+})
