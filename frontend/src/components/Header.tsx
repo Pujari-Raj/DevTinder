b8bbe44511b3b096065d3d9add8ee53a036d129b
Basvaraj Pujari <basvarajpujari607@gmail.com>
2026-04-10 23:28:05 +0530

    add: feed page added, changes in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,38 +1,50 @@
 import { Link } from "react-router-dom";
 import { FaCode } from "react-icons/fa";
+import { useAuthStore } from "../store/authStore";
 
 export default function Header() {
+  const { isAuthenticated, user } = useAuthStore();
+  console.log("user",user)
+
+  const userName = user?.name
+
   return (
     <header className="sticky top-0 z-50 bg-black border-b border-gray-800 shadow-lg">
       <nav className="navbar px-4 lg:px-8">
         <div className="flex-1">
-          <Link 
-            to="/" 
+          <Link
+            to="/"
             className="flex items-center gap-2 text-2xl font-bold text-white hover:text-sky-400 transition"
           >
             <FaCode className="text-sky-500" />
             DevTinder
           </Link>
         </div>
-        
+
         <div className="flex-none gap-4">
           <ul className="menu menu-horizontal px-1 gap-2">
             <li>
-              <Link 
-                to="/" 
+              <Link
+                to="/"
                 className="text-gray-300 hover:text-white hover:bg-gray-900 transition rounded-lg"
               >
                 Home
               </Link>
             </li>
-            <li>
-              <Link 
-                to="/login" 
-                className="text-gray-300 hover:text-white hover:bg-gray-900 transition rounded-lg"
-              >
-                Login
-              </Link>
-            </li>
+            {isAuthenticated ? (
+              <li>
+                Welcome , {userName}
+              </li>
+            ) : (
+              <li>
+                <Link
+                  to="/login"
+                  className="text-gray-300 hover:text-white hover:bg-gray-900 transition rounded-lg"
+                >
+                  Login
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
       </nav>
---
+++ frontend/src/pages/Feed.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div>
+        <p>will show list of users</p>
+    </div>
+  )
+}
+
+export default Feed
