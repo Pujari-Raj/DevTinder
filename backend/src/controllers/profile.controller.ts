1c394f0007a5276557dbe107f8efc746ee4b74b4
Pujari Basvaraj <basvarajpujari607@gmail.com>
2026-06-14 23:31:28 +0530

    add: created profile edit api

--- backend/src/controllers/profile.controller.ts
+++ backend/src/controllers/profile.controller.ts
@@ -1,22 +1,55 @@
-import { Response } from 'express';
-import { AsyncHandler } from '../utils/handlers';
-import { ApiResponse } from '../@types/type';
-
+import { Response } from "express";
+import { AsyncHandler } from "../utils/handlers";
+import { ApiResponse } from "../@types/type";
+import {
+  EditProfileSchema,
+  EditProfileSchemaType,
+} from "../validators/profile.schema";
 
 // get Profile details
-const viewProfile = AsyncHandler(async (req , res: Response<ApiResponse>) => {
-    // Getting loggedIn user's data
-    const loggedInUser = req?.user;
+const viewProfile = AsyncHandler(async (req, res: Response<ApiResponse>) => {
+  // Getting loggedIn user's data
+  const loggedInUser = req?.user;
 
-    // excluding sensitive data
-    loggedInUser.password = undefined!;
+  // excluding sensitive data
+  loggedInUser.password = undefined!;
 
-    // Return the response
-    res.status(200).json({
-        success: true,
-        message: "Profile Details fetched successfully",
-        data: loggedInUser
-    })
-})
+  // Return the response
+  res.status(200).json({
+    success: true,
+    message: "Profile Details fetched successfully",
+    data: loggedInUser,
+  });
+});
 
-export { viewProfile }
+// edit profile details
+
+const editProfile = AsyncHandler(async (req, res: Response<ApiResponse>) => {
+  // validating the request body
+  const validatedData =
+    await EditProfileSchema.validate(req?.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+  // Getting loggedIn user's data
+  const loggedInUser = req?.user;
+
+  // Updating the fields
+  Object.assign(loggedInUser, validatedData)
+
+  // saving the updated data against user
+  await loggedInUser.save();
+
+  // excluding sensitive data
+  loggedInUser.password = undefined!;
+
+  // Returning response
+  res.status(200).json({
+    success: true,
+    message: "Edited Profile Details Successfully",
+    data: loggedInUser
+  })
+});
+
+export { viewProfile, editProfile };
--- backend/src/routes/profile.route.ts
+++ backend/src/routes/profile.route.ts
@@ -1,8 +1,9 @@
-import { Router } from 'express';
-import { userAuth } from '../middlewares/auth.middleware';
-import { viewProfile } from '../controllers/profile.controller';
+import { Router } from "express";
+import { userAuth } from "../middlewares/auth.middleware";
+import { viewProfile, editProfile } from "../controllers/profile.controller";
 
 const profileRouter = Router();
 profileRouter.get("/view", userAuth, viewProfile);
+profileRouter.patch("/update", userAuth, editProfile);
 
 export default profileRouter;
